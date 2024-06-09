0c32ced8820547b856cbaf449fe25f4ab3f0c286
athenawu3 <athenawu@utexas.edu>
2024-06-09 15:20:33 -0700

    Add folder with new changes

--- PyPoll/poll.py
+++ PyPoll/poll.py
@@ -1,51 +1,63 @@
-# import and read dataset
+import os
+import csv
 
-import pandas as pd
-file_path = 'Resources/election_data.csv'
-df = pd.read_csv(file_path)
+# path to CSV file
+file_path = os.path.join('..', 'PyPoll', 'Resources', 'election_data.csv')
 
-# print
+# read CSV file
+with open (file_path, encoding='UTF-8') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    next(csv_reader)
+    data = list(csv_reader)
 
-print(" ")
-print("Election Results")
-print("-------------------------")
+    # print
+    print(" ")
+    print("Election Results")
+    print("-------------------------")
 
-## calculations
+    ## initialize lists and variables
+    vote_count = 0
+    candidate_list = []
+    prev_candidates = []
+    percentage_list = []
 
-# total number of votes cast
+    # total number of votes
+    for row in data:
+        vote_count = vote_count + 1
+    print(f"Total Votes: {vote_count}")
+    print("-------------------------")
 
-total_votes = df['Ballot ID'].count()
-print(f"Total Votes: {total_votes}")
-print("-------------------------")
+    # candidate list
+    for row in data:
 
-# complete list of candidates who received votes
+        candidate = row[2]
+        if candidate not in prev_candidates:
+            candidate_list.append(candidate)
+            prev_candidates.append(candidate)
 
-number_candidates = df['Candidate'].nunique()
-candidate = df['Candidate'].unique()
-candidate_list = candidate.tolist()
+    # number and % of votes per candidate
+    length = len(candidate_list)
+    for i in range(length):
+        candidate_count = 0
+        for row in data:
+            if row[2] == candidate_list[i]:
+                candidate_count = candidate_count + 1
+        candidate_percentage = candidate_count / vote_count
+        percentage = "{:.3%}".format(candidate_percentage)
+        print(f"{candidate_list[i]}: {percentage} ({candidate_count})")
+        percentage_list.append(candidate_percentage)
 
-## I am purposefully using a for loop so that potentially if this dataset
-## had way more than three candidates, this code could still apply
+    # winner of election based on popular vote
+    winner_percentage = max(percentage_list)
+    winner_index = percentage_list.index(winner_percentage)
+    winner_candidate = candidate_list[winner_index]
 
-# percentage and number of votes each candidate won
-# put percentages into new list to calculate winner later
-
-df['Percentage'] = ""
-
-for i in range(number_candidates):
-    candidate_count = (df['Candidate'] == candidate_list[i]).sum()
-    candidate_percentage = candidate_count / total_votes
-    percentage = "{:.3%}".format(candidate_percentage)
-    print(f"{candidate_list[i]}: {percentage} ({candidate_count})")
-
-    df.iloc[i, df.columns.get_loc('Percentage')] = percentage
-
-# winner of election based on popular vote
-winner_index = df['Percentage'].idxmax()
-winner = candidate_list[winner_index]
-print("-------------------------")
-print(f"Winner: {winner}")
-print("-------------------------")
+    #print
+    print("-------------------------")
+    print(f"Winner: {winner_candidate}")
+    print("-------------------------")
+        
+        
 
 
 
